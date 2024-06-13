32f4dab7c80553654da9c8d4b713406e3bd1f1ce
Chukwuezi Benjamin Chimaobi <benjaminchima072@gmail.com>
2024-06-13 01:24:58 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{js}","./views/*.{html,ejs}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        archivo:["Archivo", "sans-serif"]
+      },
+      screens:{
+        'tablet':{'max':'1023px'},
+        'mobile1':{'max':'583px'}
+      }
+    },
+  },
+  plugins: [],
+}
+
